e5c3c46edaf632f5a3032f81ffc5430c7f7a2841
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2024-02-12 18:16:35 +0100

    color code refactor
    Signed-off-by: jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>

--- scripts/segm-paper/iou_oracle_threshs/one_model_one_dataset_boxplot.py
+++ scripts/segm-paper/iou_oracle_threshs/one_model_one_dataset_boxplot.py
@@ -16,9 +16,7 @@ from pathlib import Path
 
 import matplotlib as mpl
 import numpy as np
-import torch
 from matplotlib import pyplot as plt
-from PIL import Image
 
 # is it running as a notebook or as a script?
 if (arg0 := Path(sys.argv[0]).stem) == "ipykernel_launcher":
@@ -80,148 +78,14 @@ else:
 print(f"{args=}")
 
 # %%
-# Load `asmaps.pt`
 
-print("loading asmaps.pt")
-
-assert (asmaps_path := args.rundir / "asmaps.pt").exists(), str(asmaps_path)  # noqa: RUF018
-
-asmaps_dict = torch.load(asmaps_path)
-assert isinstance(asmaps_dict, dict), f"{type(asmaps_dict)=}"
-
-asmaps = asmaps_dict["asmaps"]
-assert isinstance(asmaps, torch.Tensor), f"{type(asmaps)=}"
-assert asmaps.ndim == 3, f"{asmaps.shape=}"
-print(f"{asmaps.shape=}")
-
-images_relpaths = asmaps_dict["paths"]
-assert isinstance(images_relpaths, list), f"{type(images_relpaths)=}"
-
-assert len(asmaps) == len(images_relpaths), f"{len(asmaps)=}, {len(images_relpaths)=}"
-
-# collection [of datasets] = {mvtec, visa}
-collection = {p.split("/")[0] for p in images_relpaths}
-assert collection.issubset(ACCEPTED_COLLECTIONS), f"{collection=}"
-
-collection = collection.pop()
-
-if collection == MVTEC_DIR_NAME:
-    assert args.mvtec_root is not None, "please provide the argument `--mvtec-root`"
-    collection_root = args.mvtec_root
-
-if collection == VISA_DIR_NAME:
-    assert args.visa_root is not None, "please provide the argument `--visa-root`"
-    collection_root = args.visa_root
-
-assert collection_root.exists(), f"{collection=} {collection_root=!s}"
-
-dataset = {"/".join(p.split("/")[:2]) for p in images_relpaths}
-assert len(dataset) == 1, f"{dataset=}"
-
-dataset = dataset.pop()
-print(f"{dataset=}")
-
-print("sorting images and their asmaps")
-images_argsort = np.argsort(images_relpaths)
-images_relpaths = np.array(images_relpaths)[images_argsort].tolist()
-asmaps = asmaps[images_argsort]
-
-print("getting masks paths from images paths")
-
-
-def _image_path_2_mask_path(image_path: str) -> str | None:
-    if "good" in image_path:
-        # there is no mask for the normal images
-        return None
-
-    path = Path(image_path.replace("test", "ground_truth"))
-
-    if (collection := path.parts[0]) == VISA_DIR_NAME:
-        path = path.with_suffix(".png")
-
-    elif collection == MVTEC_DIR_NAME:
-        path = path.with_stem(path.stem + "_mask").with_suffix(".png")
-
-    else:
-        msg = f"Unknown collection: {collection=}"
-        raise NotImplementedError(msg)
-
-    return str(path)
-
-
-masks_relpaths = [_image_path_2_mask_path(p) for p in images_relpaths]
-
-print(f"converting relative paths to absolute paths\n{collection_root=!s}")
-
-
-def _convert_path(relative_path: str, collection_root: Path) -> str | None:
-    if relative_path is None:
-        return None
-    relative_path = Path(*Path(relative_path).parts[1:])
-    return str(collection_root / relative_path)
-
-
-images_abspaths = [_convert_path(p, collection_root) for p in images_relpaths]
-masks_abspaths = [_convert_path(p, collection_root) for p in masks_relpaths]
-
-for path in images_abspaths + masks_abspaths:
-    assert path is None or Path(path).exists(), path
-
-# %%
-# Load masks
-
-print("loading masks")
-masks_pils = [Image.open(p).convert("L") if p is not None else None for p in masks_abspaths]
-
-masks_resolution = {p.size for p in masks_pils if p is not None}
-assert len(masks_resolution) == 1, f"assumed single-resolution dataset but found {masks_resolution=}"
-masks_resolution = masks_resolution.pop()
-masks_resolution = (masks_resolution[1], masks_resolution[0])  # [W, H] --> [H, W]
-print(f"{masks_resolution=} (HEIGHT, WIDTH)")
-
-masks = torch.stack(
-    [
-        torch.tensor(np.asarray(pil), dtype=torch.bool)
-        if pil is not None
-        else torch.zeros(masks_resolution, dtype=torch.bool)
-        for pil in masks_pils
-    ],
-    dim=0,
-)
-print(f"{masks.shape=}")
-
-# %%
-# Resize asmaps to match the resolution of the masks
-
-asmaps_resolution = asmaps.shape[-2:]
-print(f"{asmaps_resolution=} (HEIGHT, WIDTH)")
-
-if asmaps_resolution == masks_resolution:
-    print("asmaps and masks have the same resolution")
-else:
-    print("resizing asmaps to match the resolution of the masks")
-    asmaps = torch.nn.functional.interpolate(
-        asmaps.unsqueeze(1),
-        size=masks_resolution,
-        mode="bilinear",
-    ).squeeze(1)
-    print(f"{asmaps.shape=}")
-
-# %%
-# Move data to device
-
-if args.device == "cpu":
-    print("using CPU")
-
-elif args.device in ("cuda", "gpu"):
-    print("moving data to GPU")
-    masks = masks.cuda()
-    asmaps = asmaps.cuda()
+# verify that rundir contains a `asmaps.pt` file
+if not (asmaps_pt := args.rundir / "asmaps.pt").is_file():
+    msg = f"It looks like the rundir is not valid. {args.rundir=}"
+    raise ValueError(msg)
 
 else:
-    msg = f"Unknown device: {args.device=}"
-    raise NotImplementedError(msg)
-
+    print("rundir looks good")
 
 # %%
 # load iou stuff
@@ -259,10 +123,23 @@ fig, axes = plt.subplots(
     layout="constrained",
 )
 
+
+def color_code_boxplot(boxplots_dict: dict, bplot_idx: int, color: str):
+    boxplots_dict["boxes"][bplot_idx].set_edgecolor(color)
+    boxplots_dict["fliers"][bplot_idx].set(markeredgecolor=color, markerfacecolor="white")
+    boxplots_dict["means"][bplot_idx].set(markeredgecolor=color, markerfacecolor="white")
+    bplot_idx_pairs = slice(bplot_idx * 2, (bplot_idx + 1) * 2)
+    for patch in (
+        boxplots_dict["whiskers"][bplot_idx_pairs]
+        + boxplots_dict["caps"][bplot_idx_pairs]
+        + [boxplots_dict["medians"][bplot_idx]]
+    ):
+        patch.set_color(color)
+
 # --------------------- without min thresh ---------------------
 # ------- global oracle
 ax = axes[0]
-_ = ax.boxplot(
+boxplots_vs = ax.boxplot(
     [
         max_avg_iou_result.ious_at_thresh[max_avg_iou_result.image_classes == 1],
         max_ious_result.ious[max_ious_result.image_classes == 1],
@@ -276,17 +153,16 @@ _ = ax.boxplot(
             vert=False,
             notch=False,
             showmeans=True,
-            meanprops={"marker": "d"},
+            meanprops={"marker": "d", "markerfacecolor": "white"},
             # make it possible to color the box with white (not transparent)
             patch_artist=True,
-            boxprops={
-                "color": "black",
-                "facecolor": "white",
-            },
+            boxprops={"facecolor": "white"},
             widths=0.5,
         )
     ),
 )
+color_code_boxplot(boxplots_vs, 0, "tab:red")
+color_code_boxplot(boxplots_vs, 1, "tab:blue")
 _ = ax.set_xlim(0 - (eps := 1e-2), 1 + eps)
 _ = ax.set_xticks(np.linspace(0, 1, 5))
 _ = ax.xaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1))
@@ -294,28 +170,29 @@ _ = ax.set_title("without validation")
 
 # ------- diff
 ax = axes[1]
-_ = ax.boxplot(
+boxplots_diff = ax.boxplot(
     [diff[max_ious_result.image_classes == 1]],
     labels=["local - global"],
     **(
         bp_kwargs_diff := dict(  # noqa: C408
             vert=False,
             notch=False,
+            showmeans=True,
+            meanprops={"marker": "d", "markerfacecolor": "white"},
+            # make it possible to color the box with white (not transparent)
             patch_artist=True,
-            boxprops={
-                "color": "black",
-                "facecolor": "white",
-            },
+            boxprops={"facecolor": "white"},
             widths=0.5,
         )
     ),
 )
+color_code_boxplot(boxplots_diff, 0, "black")
 _ = ax.xaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1))
 
 # --------------------- WITH min thresh ---------------------
 # ------- global oracle
 ax = axes[2]
-_ = ax.boxplot(
+boxplots_vs = ax.boxplot(
     [
         max_avg_iou_min_thresh_result.ious_at_thresh[max_avg_iou_result.image_classes == 1],
         max_ious_min_thresh_result.ious[max_ious_result.image_classes == 1],
@@ -326,6 +203,8 @@ _ = ax.boxplot(
     ],
     **bp_kwargs_vs,
 )
+color_code_boxplot(boxplots_vs, 0, "tab:red")
+color_code_boxplot(boxplots_vs, 1, "tab:blue")
 _ = ax.set_xlim(0 - (eps := 1e-2), 1 + eps)
 _ = ax.set_xticks(np.linspace(0, 1, 5))
 _ = ax.xaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1))
@@ -333,11 +212,12 @@ _ = ax.set_title("with min thresh validation")
 
 # ------- diff
 ax = axes[3]
-_ = ax.boxplot(
+boxplots_diff = ax.boxplot(
     [diff_min_thresh[max_ious_result.image_classes == 1]],
     labels=["local - global"],
     **bp_kwargs_diff,
 )
+color_code_boxplot(boxplots_diff, 0, "black")
 _ = ax.xaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1))
 
 
--- scripts/segm-paper/iou_oracle_threshs/one_model_one_dataset_curves.py
+++ scripts/segm-paper/iou_oracle_threshs/one_model_one_dataset_curves.py
@@ -78,148 +78,14 @@ else:
 print(f"{args=}")
 
 # %%
-# Load `asmaps.pt`
 
-print("loading asmaps.pt")
-
-assert (asmaps_path := args.rundir / "asmaps.pt").exists(), str(asmaps_path)  # noqa: RUF018
-
-asmaps_dict = torch.load(asmaps_path)
-assert isinstance(asmaps_dict, dict), f"{type(asmaps_dict)=}"
-
-asmaps = asmaps_dict["asmaps"]
-assert isinstance(asmaps, torch.Tensor), f"{type(asmaps)=}"
-assert asmaps.ndim == 3, f"{asmaps.shape=}"
-print(f"{asmaps.shape=}")
-
-images_relpaths = asmaps_dict["paths"]
-assert isinstance(images_relpaths, list), f"{type(images_relpaths)=}"
-
-assert len(asmaps) == len(images_relpaths), f"{len(asmaps)=}, {len(images_relpaths)=}"
-
-# collection [of datasets] = {mvtec, visa}
-collection = {p.split("/")[0] for p in images_relpaths}
-assert collection.issubset(ACCEPTED_COLLECTIONS), f"{collection=}"
-
-collection = collection.pop()
-
-if collection == MVTEC_DIR_NAME:
-    assert args.mvtec_root is not None, "please provide the argument `--mvtec-root`"
-    collection_root = args.mvtec_root
-
-if collection == VISA_DIR_NAME:
-    assert args.visa_root is not None, "please provide the argument `--visa-root`"
-    collection_root = args.visa_root
-
-assert collection_root.exists(), f"{collection=} {collection_root=!s}"
-
-dataset = {"/".join(p.split("/")[:2]) for p in images_relpaths}
-assert len(dataset) == 1, f"{dataset=}"
-
-dataset = dataset.pop()
-print(f"{dataset=}")
-
-print("sorting images and their asmaps")
-images_argsort = np.argsort(images_relpaths)
-images_relpaths = np.array(images_relpaths)[images_argsort].tolist()
-asmaps = asmaps[images_argsort]
-
-print("getting masks paths from images paths")
-
-
-def _image_path_2_mask_path(image_path: str) -> str | None:
-    if "good" in image_path:
-        # there is no mask for the normal images
-        return None
-
-    path = Path(image_path.replace("test", "ground_truth"))
-
-    if (collection := path.parts[0]) == VISA_DIR_NAME:
-        path = path.with_suffix(".png")
-
-    elif collection == MVTEC_DIR_NAME:
-        path = path.with_stem(path.stem + "_mask").with_suffix(".png")
-
-    else:
-        msg = f"Unknown collection: {collection=}"
-        raise NotImplementedError(msg)
-
-    return str(path)
-
-
-masks_relpaths = [_image_path_2_mask_path(p) for p in images_relpaths]
-
-print(f"converting relative paths to absolute paths\n{collection_root=!s}")
-
-
-def _convert_path(relative_path: str, collection_root: Path) -> str | None:
-    if relative_path is None:
-        return None
-    relative_path = Path(*Path(relative_path).parts[1:])
-    return str(collection_root / relative_path)
-
-
-images_abspaths = [_convert_path(p, collection_root) for p in images_relpaths]
-masks_abspaths = [_convert_path(p, collection_root) for p in masks_relpaths]
-
-for path in images_abspaths + masks_abspaths:
-    assert path is None or Path(path).exists(), path
-
-# %%
-# Load masks
-
-print("loading masks")
-masks_pils = [Image.open(p).convert("L") if p is not None else None for p in masks_abspaths]
-
-masks_resolution = {p.size for p in masks_pils if p is not None}
-assert len(masks_resolution) == 1, f"assumed single-resolution dataset but found {masks_resolution=}"
-masks_resolution = masks_resolution.pop()
-masks_resolution = (masks_resolution[1], masks_resolution[0])  # [W, H] --> [H, W]
-print(f"{masks_resolution=} (HEIGHT, WIDTH)")
-
-masks = torch.stack(
-    [
-        torch.tensor(np.asarray(pil), dtype=torch.bool)
-        if pil is not None
-        else torch.zeros(masks_resolution, dtype=torch.bool)
-        for pil in masks_pils
-    ],
-    dim=0,
-)
-print(f"{masks.shape=}")
-
-# %%
-# Resize asmaps to match the resolution of the masks
-
-asmaps_resolution = asmaps.shape[-2:]
-print(f"{asmaps_resolution=} (HEIGHT, WIDTH)")
-
-if asmaps_resolution == masks_resolution:
-    print("asmaps and masks have the same resolution")
-else:
-    print("resizing asmaps to match the resolution of the masks")
-    asmaps = torch.nn.functional.interpolate(
-        asmaps.unsqueeze(1),
-        size=masks_resolution,
-        mode="bilinear",
-    ).squeeze(1)
-    print(f"{asmaps.shape=}")
-
-# %%
-# Move data to device
-
-if args.device == "cpu":
-    print("using CPU")
-
-elif args.device in ("cuda", "gpu"):
-    print("moving data to GPU")
-    masks = masks.cuda()
-    asmaps = asmaps.cuda()
+# verify that rundir contains a `asmaps.pt` file
+if not (asmaps_pt := args.rundir / "asmaps.pt").is_file():
+    msg = f"It looks like the rundir is not valid. {args.rundir=}"
+    raise ValueError(msg)
 
 else:
-    msg = f"Unknown device: {args.device=}"
-    raise NotImplementedError(msg)
-
+    print("rundir looks good")
 
 # %%
 # load iou stuff
@@ -261,7 +127,9 @@ _ = ioucurves_global.scatter_global_oracle(
     max_avg_iou_min_thresh_result.avg_iou,
 )
 
-_ = ax.legend(loc="upper left", framealpha=1, fontsize="small")
+leg = ax.legend(loc="upper left", framealpha=1, fontsize="small")
+for line in leg.get_lines():
+    line.set_linewidth(2.5)
 
 # %%
 # diff stats curves
@@ -270,12 +138,11 @@ fig, ax = plt.subplots(figsize=np.array((6, 4)))
 
 for bp_stat, color in zip(diff_stats, ["tab:orange", "tab:purple", "tab:brown", "tab:cyan"], strict=True):
     iou_curve = ioucurves_global.per_image_ious[bp_stat["image_idx"]]
-    line = ioucurves_global.plot_iou_curve(ax, bp_stat["image_idx"], color=color)[0]
+    _ = ioucurves_global.plot_iou_curve(ax, bp_stat["image_idx"], color=color)
     _ = ioucurves_global.scatter_local_oracle(
         ax,
         max_ious_result.threshs[bp_stat["image_idx"]],
         max_ious_result.ious[bp_stat["image_idx"]],
-        color=line.get_color(),
     )
 _ = ioucurves_global.axvline_min_thresh(ax, max_avg_iou_min_thresh_result.min_thresh)
 _ = ioucurves_global.axvline_global_oracle(ax, max_avg_iou_min_thresh_result.thresh)
